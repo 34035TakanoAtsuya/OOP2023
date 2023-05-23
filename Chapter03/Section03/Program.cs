72b8bd0e6d553b41e80fd54cdb36c75343edba4a
髙野 敦也 <OIBC34035@ojs.ac.jp>
2023-05-23 11:12:55 +0900

    P90

---
+++ Chapter03/Section03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //var exists = list.Exists(s => s[2] == 'k');
+            //Console.WriteLine(exists);
+
+            //var Index = list.FindIndex(s => s == "Canberra");
+            //Console.WriteLine(Index);
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
