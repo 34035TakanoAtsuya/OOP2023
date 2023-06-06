18871a54a93e2717aa359fa8ce93ec7ac35c09dc
髙野 敦也 <OIBC34035@ojs.ac.jp>
2023-06-06 09:45:32 +0900

    P159問題5.4

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -7,7 +7,14 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+
+            var lines = line.Replace("Novelist", "作家").Replace("BestWork", "代表作").Replace("Born","誕生年").Split(';');
+
+            foreach (var s in lines) {
+                Console.WriteLine(s);
+            }
         }
     }
 }
