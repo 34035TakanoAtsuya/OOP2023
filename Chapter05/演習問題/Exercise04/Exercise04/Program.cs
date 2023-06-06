0df313b9cad8fa02956719456652e2fada0834b1
髙野 敦也 <OIBC34035@ojs.ac.jp>
2023-06-06 10:38:17 +0900

    P159演習問題5.4Array

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,14 +8,35 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-
+#if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+#elif StringArray
+            Stopwatch sw = new Stopwatch(); //タイマーインスタンス生成
+            sw.Start(); //タイマースタート
+            string[] line = {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896"
+            };
+#endif
 
-            var lines = line.Replace("Novelist", "作家").Replace("BestWork", "代表作").Replace("Born","誕生年").Split(';');
+#if NonArray
+            var lines = line.Replace("Novelist", "作家").Replace("BestWork", "代表作").Replace("Born","誕生年").Replace("=",":").Split(';');
 
             foreach (var s in lines) {
                 Console.WriteLine(s);
             }
+#elif StringArray
+            for (int i = 0; i < line.Length; i++) {
+
+                var lines = line[i].Replace("Novelist", "作家").Replace("BestWork", "代表作").Replace("Born", "誕生年").Replace("=",":").Split(';');
+                foreach (var s in lines) {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine("実行時間 = {0}",sw.Elapsed.ToString(@"ss\.fffff"));  //時間表示
+#endif
         }
     }
 }
