735bee21de384cfb83070f43a830516922e39009
髙野 敦也 <OIBC34035@ojs.ac.jp>
2023-06-30 10:55:30 +0900

    7章演習問題

---
+++ Chapter07/演習問題/Section02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+
+            //Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //7.2.3
+            //上のaddメソッドで2つのオブジェクトで追加して読み込む
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            //7.2.3(Removeの呼び出し)
+            if (abbrs.Remove("NPT"))
+                Console.WriteLine(abbrs.Count);
+            if (!abbrs.Remove("NPT"))
+                Console.WriteLine("削除できません。");
+            Console.WriteLine();
+
+            //7.2.4
+            //IEnumerableを実装したのでLINQが使える
+            foreach (var item in abbrs.Where(abb => abb.Key.Length == 3)) {
+                Console.WriteLine("{0}={1}",item.Key,item.Value);
+            }
+        }
+    }
+}
