8df348354379e99c86cdbe85c53160cd43d7f97e
髙野 敦也 <OIBC34035@ojs.ac.jp>
2023-10-05 11:18:49 +0900

    1005Exercise03

--- Chapter10/演習問題/Exercise03/Program.cs
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 
 namespace Exercise03 {
     class Program {
@@ -13,10 +15,12 @@ namespace Exercise03 {
                "It will take time.",
                "We reorganized the timetable.",
             };
-
-
-
-
+            foreach (var line in texts) {
+                var matches = Regex.Matches(line,@"(T|t)ime\b");
+                foreach (Match item in matches) {
+                    Console.WriteLine("{0}: {1}",line,item.Index);
+                }
+            }
         }
     }
 }
