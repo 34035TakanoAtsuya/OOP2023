1249495363a4bd191b9e2585683c2554de3d116d
髙野 敦也 <OIBC34035@ojs.ac.jp>
2023-10-11 11:32:21 +0900

    15章Section01

--- Chapter15/Section01/Properties/AssemblyInfo.cs
+++ Chapter15/Section01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Section01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Section01")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("52c73958-73e7-4032-8662-dbafd6084ba5")]
+[assembly: Guid("264f5e87-18ab-42c0-941d-efdd3e04707b")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
